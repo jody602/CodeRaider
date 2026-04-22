b2ead37aa8a2515dd6d24819fe63867a8b8f4088
Jordan Ibrahim <62116280+jody602@users.noreply.github.com>
2026-04-22 01:05:36 -0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
- export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyDV-x32I0SyLiA1Sphp4a2lgjYMCKzwAeg",
-    authDomain: "codeasehg.firebaseapp.com",
-    databaseURL: "https://codeasehg-default-rtdb.firebaseio.com",
-    projectId: "codeasehg",
-    storageBucket: "codeasehg.firebasestorage.app",
-    messagingSenderId: "898513503272",
-    appId: "1:898513503272:web:4d6e6a9673f24281d5e11d"
-  };
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDV-x32I0SyLiA1Sphp4a2lgjYMCKzwAeg",
+  authDomain: "codeasehg.firebaseapp.com",
+  databaseURL: "https://codeasehg-default-rtdb.firebaseio.com",
+  projectId: "codeasehg",
+  storageBucket: "codeasehg.firebasestorage.app",
+  messagingSenderId: "898513503272",
+  appId: "1:898513503272:web:4d6e6a9673f24281d5e11d"
+};
